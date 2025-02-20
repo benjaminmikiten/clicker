b9b7af1c3ff84b3444dcd165ffd69f9bdfb596cc
Benjamin Mikiten <benjaminmikiten@gmail.com>
2025-02-19 21:00:50 -0600

    chore(rename): renames from ultimate to clicker

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from "@nestjs/core";
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from "@nestjs/platform-fastify";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from "./app.module.js";
 
-const app = await NestFactory.create<NestFastifyApplication>(
-  AppModule,
-  new FastifyAdapter()
-);
+const app = await NestFactory.create(AppModule);
+
+// Swagger setup
+const config = new DocumentBuilder()
+  .setTitle('Clicker API')
+  .setVersion('1.0')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
 
 await app.listen(3000, "0.0.0.0");
