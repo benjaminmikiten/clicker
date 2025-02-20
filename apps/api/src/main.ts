cb05c0ff94139ec9d5a6d770c93afd0772833f35
Benjamin Mikiten <benjaminmikiten@gmail.com>
2025-02-19 21:25:11 -0600

    chore: update deps, husky

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,16 +1,13 @@
-import { NestFactory } from "@nestjs/core";
+import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-import { AppModule } from "./app.module.js";
+import { AppModule } from './app.module.js';
 
 const app = await NestFactory.create(AppModule);
 
 // Swagger setup
-const config = new DocumentBuilder()
-  .setTitle('Clicker API')
-  .setVersion('1.0')
-  .build();
+const config = new DocumentBuilder().setTitle('Clicker API').setVersion('1.0').build();
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
 
-await app.listen(3000, "0.0.0.0");
+await app.listen(3001, '0.0.0.0');
